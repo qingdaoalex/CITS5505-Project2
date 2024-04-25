b192eb3b3a13eb20be93f3e93f0b2b6d29760dd4
alexuwa <alexuwa@qdAlexPro16.local>
2024-04-26 00:37:46 +0800

    book detail static page

---
+++ Views/bookDetail/script.js
@@ -0,0 +1,26 @@
+$(".button--approve").click(function () {
+    $(this).toggleClass("active");
+    $(this).siblings('.button--deny').removeClass("active");
+});
+
+$(".button--deny").click(function () {
+    $(this).toggleClass("active");
+    $(this).siblings('.button--approve').removeClass("active");
+});
+
+$(".comment-trigger").click(function () {
+    $(this).parent().parent().toggleClass("post--commenting");
+});
+
+$(".button--flag").click(function () {
+    $(this).parent().parent().toggleClass("post--commenting");
+});
+
+
+$(".button--confirm").click(function () {
+    $(this).parent().parent().parent().parent().parent().toggleClass("post--commenting");
+});
+
+$(".button.cancel").click(function () {
+    $(this).parent().parent().parent().parent().parent().toggleClass("post--commenting");
+});
