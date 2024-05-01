a597e60401954450ca50032c629c53a0b36be06e
Yunfang <l98lavie@gmail.com>
2024-05-01 22:12:25 +0800

    add mail configuration

--- blogweb/config.py
+++ blogweb/config.py
@@ -5,12 +5,11 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or "smtp.qq.com"
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 465)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_DEBUG = os.environ.get('MAIL_DEBUG') == 'True'
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or "3357763382@qq.com"
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or "woddizqutfukcjja"
-    # MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER') or "qq邮箱"
+    # Mail configuration
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 465
+    MAIL_USERNAME = 'cits5505webapp@qq.com'
+    MAIL_PASSWORD = 'woddizqutfukcjja'
+    MAIL_USE_TLS = False
+    MAIL_USE_TLS = True
     ADMINS = ['cits5505webapp@qq.com']
