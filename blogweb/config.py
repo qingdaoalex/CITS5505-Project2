2839510b4546384bf3289248d2ed06d0a7b9144f
Maggiexz <145413500+Maggiexz@users.noreply.github.com>
2024-05-04 16:12:51 +0800

    make this  branch more clear

---
+++ blogweb/config.py
@@ -0,0 +1,14 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    # Mail configuration
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'cits5505webapp@qq.com'
+    MAIL_PASSWORD = 'woddizqutfukcjja'
+    ADMINS = ['l98lavie@gmail.com']
