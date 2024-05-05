d4deab3db846792865123450bd688642116810c3
Yunfang <l98lavie@gmail.com>
2024-05-05 11:58:00 +0800

    improve a tyro

--- blogweb/app/routes.py
+++ blogweb/app/routes.py
@@ -167,8 +167,8 @@ def before_request():
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
-    if form.validate_on_submit(current_user.username):
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         current_user.email = form.email.data
