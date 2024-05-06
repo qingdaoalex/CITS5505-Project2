44c45b4ad47bdbb58877a4bcbe77e37e120734d4
Maggiexz <145413500+Maggiexz@users.noreply.github.com>
2024-05-06 19:05:52 +0800

    fix the username hyperlink invalid problem and add the send message feature

--- blogweb/app/models.py
+++ blogweb/app/models.py
@@ -34,6 +34,7 @@ class User(UserMixin, db.Model):
   about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
   last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
     default=lambda: datetime.now())
+  last_message_read_time: so.Mapped[Optional[datetime]]
   
   following: so.WriteOnlyMapped['User'] = so.relationship(
     secondary=followers, primaryjoin=(followers.c.follower_id == id),
@@ -43,6 +44,8 @@ class User(UserMixin, db.Model):
     secondary=followers, primaryjoin=(followers.c.followed_id == id),
     secondaryjoin=(followers.c.follower_id == id),
     back_populates='following', passive_deletes=True)
+  messages_sent: so.WriteOnlyMapped['Message'] = so.relationship( foreign_keys='Message.sender_id', back_populates='author')
+  messages_received: so.WriteOnlyMapped['Message'] = so.relationship( foreign_keys='Message.recipient_id', back_populates='recipient')
 
   def __repr__(self):
     return '<User {}>'.format(self.username)
@@ -58,6 +61,11 @@ class User(UserMixin, db.Model):
       {'reset_password': self.id, 'exp': time() + expires_in},
       app.config['SECRET_KEY'], algorithm='HS256')
 
+  def unread_message_count(self):
+    last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+    query = sa.select(Message).where(Message.recipient == self, Message.timestamp > last_read_time)
+    return db.session.scalar(sa.select(sa.func.count()).select_from(query.subquery()))
+  
   @staticmethod
   def verify_reset_password_token(token):
     try:
@@ -110,6 +118,8 @@ class User(UserMixin, db.Model):
       .group_by(Post)
       .order_by(Post.timestamp.desc())
     )
+  
+
 
 class Post(db.Model):
   id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -128,3 +138,15 @@ class Post(db.Model):
 @login.user_loader
 def load_user(id):
   return db.session.get(User, int(id))
+
+class Message(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    sender_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    recipient_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column( index=True, default=lambda: datetime.now())
+    author: so.Mapped[User] = so.relationship( foreign_keys='Message.sender_id', back_populates='messages_sent')
+    recipient: so.Mapped[User] = so.relationship( foreign_keys='Message.recipient_id', back_populates='messages_received')
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
