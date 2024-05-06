6defee7f86d24cc2e7f269211274acb559620138
Yunfang <l98lavie@gmail.com>
2024-05-06 23:02:16 +0800

    changes live2d.js

--- blogweb/app/static/live2D.js
+++ blogweb/app/static/live2D.js
@@ -1,30 +1,34 @@
 
-L2Dwidget.init({
+L2Dwidget
+  .on('*', (name) => {
+    console.log('%c EVENT ' + '%c -> ' + name, 'background: #222; color: yellow', 'background: #fff; color: #000')
+  })
+  .init({
     "model": {
-        "jsonPath": "https://unpkg.com/live2d-widget-model-hijiki@1.0.5/assets/hijiki.model.json",
-        "scale": 1
+      "jsonPath": "https://unpkg.com/live2d-widget-model-hijiki@1.0.5/assets/hijiki.model.json",
+      "scale": 1
     },
     "display": {
-        "position":"right",
-        "width": 300,
-        "height": 400,
-        "hOffset": 0,
-        "vOffset": -20
+      "position": "right",
+      "width": 300,
+      "height": 400,
+      "hOffset": 0,
+      "vOffset": -20
     },
-    "dialog":{
-        "enable": true,
-        "script": {
-          'tap body': '害羞/(/ ⁄•/ω/•/ /)/',
-          'tap face': '~~'
-        }
+    "dialog": {
+      "enable": true,
+      "script": {
+        'tap body': '害羞/(/ ⁄•/ω/•/ /)/',
+        'tap face': '~~'
+      }
     },
     "mobile": {
-        "show": true,
-        "scale": 0.5
+      "show": true,
+      "scale": 0.5
     },
     "react": {
-        "opacityDefault": 0.8,
-        "opacityOnHover": 0.1
+      "opacityDefault": 0.8,
+      "opacityOnHover": 0.1
     }
-});
+  });
 
