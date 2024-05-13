eeff08cd1a5f281a97b1373b420b1e84ce41ac8a
Xiao Zhang <145413500+Maggiexz@users.noreply.github.com>
2024-05-13 22:19:40 +0800

    Update darkmode.js

--- blogweb/app/static/darkmode.js
+++ blogweb/app/static/darkmode.js
@@ -8,11 +8,11 @@ const options = {
   right: 'unset', // default: '32px'
   left: '32px', // default: 'unset'
   time: '0.5s', // default: '0.3s'
-  mixColor: '#100f2c', // default: '#fff'
-  backgroundColor: '#100f2c',  // default: '#fff'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
-  buttonColorLight: '#100f2c', // default: '#fff'
-  saveInCookies: true, // default: true,
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
   label: '🌓', // default: ''
   autoMatchOsTheme: true // default: true
 }
