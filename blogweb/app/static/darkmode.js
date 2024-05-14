85da119dff4e136d30ae0d08b11d286e6e799c1e
Maggiexz <145413500+Maggiexz@users.noreply.github.com>
2024-05-14 15:07:13 +0800

    change style

--- blogweb/app/static/darkmode.js
+++ blogweb/app/static/darkmode.js
@@ -8,13 +8,12 @@ const options = {
   right: 'unset', // default: '32px'
   left: '32px', // default: 'unset'
   time: '0.5s', // default: '0.3s'
-  mixColor: '#fff', // default: '#fff'
   backgroundColor: '#fff',  // default: '#fff'
-  buttonColorDark: '#100f2c',  // default: '#100f2c'
-  buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: false, // default: true,
+  buttonColorDark: '#fff',  // default: '#100f2c'
+  buttonColorLight: '#100f2c', // default: '#fff'
+  saveInCookies: true, // default: true,
   label: '🌓', // default: ''
-  autoMatchOsTheme: true // default: true
+  autoMatchOsTheme: false // default: true
 }
 
 const darkmode = new Darkmode(options);
