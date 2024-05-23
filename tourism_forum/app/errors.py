45be94773f4008b924a16ccbc257852b58be8807
Lavie <126407102+Yunfang-Lyu@users.noreply.github.com>
2024-05-23 20:15:06 +0800

    add error handling

--- tourism_forum/app/errors.py
+++ tourism_forum/app/errors.py
@@ -2,11 +2,11 @@ from flask import render_template
 from app import db
 from app.blueprints import main
 
-@main.errorhandler(404)
+@main.app_errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
 
-@main.errorhandler(500)
+@main.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
